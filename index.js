77c034dcaecc3381a7e1547cac369adaaf007832
Ken Bannister <kb2ma@runbox.com>
2022-01-12 17:46:08 -0500

    Refactor and add ability to delete device
    Change-type: minor
    Signed-off-by: Ken Bannister <kb2ma@runbox.com>

--- index.js
+++ index.js
@@ -5,17 +5,44 @@ import crypto from 'crypto'
 import util from 'util'
 
 const generateKeyPair = util.promisify(crypto.generateKeyPair)
+// GCP IoT Client
+let iot = null
+// Path string to IoT Core registry (project, region, registry) required for IoT functions
+let registryPath = ''
 
+/**
+ * Provides create and deletion of GCP IoT Core device and updates balena GCP_PRIVATE_KEY
+ * environment var. Uses POST request to create and DELETE to delete. Expects request
+ * body with JSON containing {uuid: <device-uuid>}.
+ */
 export async function provision(req, res) {
-    let uuid = ''
     try {
-        uuid = req.body.uuid
         const creds =  { email: process.env.RESIN_EMAIL, password: process.env.RESIN_PASSWORD }
-
         await balena.auth.login(creds)
 
         // Validate device with balenaCloud
-        await balena.models.device.get(uuid)
+        await balena.models.device.get(req.body.uuid)
+
+        // Initialize globals for GCP IoT data
+        iot = new iotApi.v1.DeviceManagerClient({
+            projectId: process.env.PROJECT_ID,
+            credentials: JSON.parse(Buffer.from(process.env.GCP_SERVICE_ACCOUNT, 'base64').toString())
+        })
+        registryPath = iot.registryPath(process.env.GCP_PROJECT_ID, process.env.GCP_REGION,
+            process.env.GCP_REGISTRY_ID)
+
+        switch (req.method) {
+            case 'POST':
+                console.log("Creating device...")
+                await handlePost(req, res, )
+                break
+            case 'DELETE':
+                console.log("Deleting device...")
+                await handleDelete(req, res)
+                break
+            default:
+                throw "method not handled"
+        }
     } catch (error) {
         console.log("Error: ", error)
         if (error.code === balena.errors.BalenaDeviceNotFound.prototype.code
@@ -25,38 +52,45 @@ export async function provision(req, res) {
             res.status(500)
         }
         res.send(error)
-        return
     }
+}
 
-    try {
-        // generate key pair; we only need the private key 
-        const keyPair = await generateKeyPair('ec', {namedCurve: 'prime256v1',
-            privateKeyEncoding: { type: 'pkcs8', format: 'pem'},
-            publicKeyEncoding: { type: 'spki', format: 'pem' }
-        })
+/**
+ * Adds device to GCP IoT registry with new key pair, and adds balena device environment
+ * var for the private key.
+ *
+ * Throws an error on failure to create the device.
+ */
+async function handlePost(req, res) {
+    // generate key pair; we only need the private key 
+    const keyPair = await generateKeyPair('ec', {namedCurve: 'prime256v1',
+        privateKeyEncoding: { type: 'pkcs8', format: 'pem'},
+        publicKeyEncoding: { type: 'spki', format: 'pem' }
+    })
 
-        // create GCP IoT device
-        let iot = new iotApi.v1.DeviceManagerClient({
-            projectId: process.env.PROJECT_ID,
-            credentials: JSON.parse(Buffer.from(process.env.GCP_SERVICE_ACCOUNT, 'base64').toString())
-        })
-
-        const path = iot.registryPath(process.env.GCP_PROJECT_ID, process.env.GCP_REGION,
-                process.env.GCP_REGISTRY_ID)
-        let device = {
-            id: `balena-${uuid}`,
-            credentials: [{ publicKey: { format: 'ES256_PEM', key: keyPair.publicKey } }]
-        }
-        await iot.createDevice({ parent: path, device: device })
-
-        // set balena device variable for the private key
-        await balena.models.device.envVar.set(uuid, 'GCP_PRIVATE_KEY',
-                Buffer.from(keyPair.privateKey).toString('base64'))
-
-        res.status(200).send("device valid")
-
-    } catch (error) {
-        console.log("Error: ", error)
-        res.status(500).send(error)
+    const deviceId = `balena-${req.body.uuid}`
+    const device = {
+        id: deviceId,
+        credentials: [{ publicKey: { format: 'ES256_PEM', key: keyPair.publicKey } }]
     }
+    await iot.createDevice({ parent: registryPath, device: device })
+
+    await balena.models.device.envVar.set(req.body.uuid, 'GCP_PRIVATE_KEY',
+            Buffer.from(keyPair.privateKey).toString('base64'))
+
+    console.log(`Created device ${deviceId}`)
+    res.status(201).send("device created")
+}
+
+/**
+ * Removes device from GCP IoT registry and balena device environment var.
+ */
+async function handleDelete(req, res) {
+    const deviceId = `balena-${req.body.uuid}`
+    await iot.deleteDevice({ name: `${registryPath}/devices/${deviceId}` })
+
+    await balena.models.device.envVar.remove(req.body.uuid, 'GCP_PRIVATE_KEY')
+
+    console.log(`Deleted device ${deviceId}`)
+    res.status(200).send("device deleted")
 }
